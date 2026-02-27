f6b5806d857aa909eb96b7bf933e727653f4ef56
TheGameTripleS <s.zaman2410@gmail.com>
2026-02-27 12:37:57 +0600

    Fronten deployed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,10 +2,13 @@ import NavBar from "./components/NavBar"
 import HomePage from "./pages/HomePage"
 import ItemPage from "./pages/ItemPage"
 import { Routes, Route } from "react-router-dom"
+import { useThemeStore } from "./store/useThemeStore"
 
 function App() {
+  const { theme } = useThemeStore()
+  
   return (
-    <div className="min-h-screen bg-base-200 transition-colors duration-300">
+    <div className="min-h-screen bg-base-200 transition-colors duration-300" data-theme={theme}>
       <NavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
